15744f9d4c40213df4ce5c031016b7af1fd6ef23
tsantek <santek.vg@gmail.com>
2019-07-06 15:56:36 -0700

    update video list

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,18 +1,15 @@
-import React from 'react'
+import './VideoItem.css';
+import React from 'react';
 
 
 const VideoItem = ({video}) =>{
     return (
-        <div className="ui list">
-            <div className="item">
-                <img className="ui avatar image" alt="" src={ video.snippet.thumbnails.medium.url} />
+            <div className="item video-item">
+                <img className="ui image" alt="" src={ video.snippet.thumbnails.medium.url} />
                 <div className="content">
-                    <p className="header">{ video.snippet.title}</p>
-                    <div className="description">Last seen watching <a><b>Arrested Development</b></a> just now.
-                    </div>
+                    <div className="header">{ video.snippet.title}</div>
                 </div>
             </div>
-        </div>
     )
 }
 
